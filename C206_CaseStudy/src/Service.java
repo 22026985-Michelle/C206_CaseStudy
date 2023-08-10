bd4a82aed896aeb16121c75c23148173910fa6f2
TY_Zhong <22044470@myrp.edu.sg>
2023-08-10 21:29:50 +0800

    Create Service class

---
+++ C206_CaseStudy/src/Service.java
@@ -0,0 +1,24 @@
+
+public class Service {
+	private int spId;
+	private String service;
+	
+	public Service(int spId, String service) {
+		this.spId = spId;
+		this.service = service;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public int getSpId() {
+		return spId;
+	}
+	
+	
+}
