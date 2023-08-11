f59143f62179caa4844635bc02cf16b871240108
TY_Zhong <22044470@myrp.edu.sg>
2023-08-11 09:53:53 +0800

    Initial Creation

---
+++ C206_CaseStudy/test/QuoteTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuoteTest {
+
+}
